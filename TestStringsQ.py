be4955a24207b2eeec43f99ceebbc8352e3590ba
The-Skas <skas@live.co.uk>
2015-01-25 17:49:46 +0000

    Implemented and Tested three String methods: anagrams, unique characters, and duplicatate characters. Following the Top 20 String Questions article.

--- TestStringsQ.py
+++ TestStringsQ.py
@@ -1,21 +1,103 @@
 from stringsq import StringsQ
+from stringsq import InvalidTypeException
 import unittest
-import pdb
+
 class TestStringsQ(unittest.TestCase):
 
-	def setUp(self):
+	def setup(self):
 		pass
 
-	def testGetDuplicateChars(self):
-		should_return_l = StringsQ.getDuplicateChars("hello")
-		self.assertEquals(should_return_l, "l")
+	def test_get_duplicate_chars(self):
+		should_return_an_empty_string = StringsQ.get_duplicate_chars("heyo")
+		self.assertEqual("", should_return_an_empty_string)
 
-		should_return_a = StringsQ.getDuplicateChars("mama")
-		self.assertEquals(should_return_a, "a")
+		should_return_l = StringsQ.get_duplicate_chars("hello")
+		self.assertEqual("l", should_return_l)
 
-def main():
-    unittest.main()
-if __name__ == '__main__':
-	main()
+		should_return_r = StringsQ.get_duplicate_chars("rawr")
+		self.assertEqual("r", should_return_r)
 
+		should_return_l = StringsQ.get_duplicate_chars("hello")
+		self.assertEqual("l", should_return_l)
+
+		self.assertRaises(InvalidTypeException, StringsQ.get_duplicate_chars,None)
+
+		self.assertRaises(InvalidTypeException, StringsQ.get_duplicate_chars, "")
+		StringsQ.get_duplicate_chars("HEYO WHATS YOUR PROBLEM")
 	
+
+	"""
+	Test Anagrams
+	"""		
+	def test_are_anagrams(self):
+		"""
+		Format of tuples is as follows
+
+			(tuple2 : str1, tuple2 : str2, tuple3 : True/False)
+
+			tuple2: string1 passed into the anagram method.
+			tuple2: string2 passed into the anagram method.
+			tuple3: the assertion of either True/False.
+
+		"""
+		anagram_input = (
+				# Assert True
+				("Hello", "olelH", 	   True),
+				("Cool" , "looC",  	   True),
+				("cool" , "looC",  	   True), #capitals shouldnt make a difference
+				("Cool" , "looC",  	   True),
+				("He1rr", "1rRhe",	   True),
+				("  " , " ",	   	   True),
+				("Army " , "Mary ",	   True),
+				("Ar m y " , "M a ry ",True),
+				# Assert False
+				("Ar m y " , "M aary ",False),
+				("Ar m y " , "M aary ",False),
+			)
+
+		def msg(x,y):
+			return "Str1: "+x+" - Str2: "+y
+
+		# Skas: Omg i'm making these these list comprehensions 
+		# look ugly :0. Laziness Prevails!
+		
+		[self.assertEqual(condition, StringsQ.are_anagrams(x,y),msg(x,y)) \
+			for x,y,condition in anagram_input] 
+
+		del msg
+
+	def test_get_first_unique_character(self):
+		"""
+		Format of tuples is as follows
+
+			(tuple1 : string, tuple2 : char1)
+
+			tuple1: String to be passed into the method
+			tuple2: Character that should be returned
+
+		"""
+		unique_input = (
+				# When unique characters found
+				("Hello",   "h"),
+				("ehlol",   "e"),
+				(" ",       " "), 
+				("+!man",   "+"),
+				# When Unique Chars NOT found
+				("hh", 		 ""),
+				("!!hhkk",   ""),
+				("hkkkkh!!", "")
+			)
+
+
+		# Runs a test on all the inputs
+		# 	Skas: I know it looks ugly :{ ! Just doing it for the sake of learning!
+		# 	
+		[self.assertEqual(StringsQ.get_first_unique_character(input_str), result_char) \
+			for input_str,result_char in unique_input] 
+
+
+		
+			
+
+if __name__ == '__main__':
+	unittest.main()
--- stringsq.py
+++ stringsq.py
@@ -1,13 +1,109 @@
 import pdb
 # Duplicate characters in a str
 # 
+class InvalidTypeException(Exception):
+	pass
 
 class StringsQ(object):
-	@classmethod
-	def getDuplicateChars(self,aStr):
-		# Make sure type is str
-		assert isinstance(aStr, str), "The value of arguement needs to be 'str' \t\ninstead its "+type(aStr)
-		return "a"
+	def __init__(self):
+		pass
 
-StringsQ.getDuplicateChars("aStr")
+	"""
+	A private class method for input sanitization
+	"""
+	@staticmethod
+	def _checkInput(aStr):
+		error_msg = "The parameter needs to be of type: 'str' \n\tinstead it is of type: "+str(type(aStr).__name__)
+		if(isinstance(aStr,str) == False):
+			raise InvalidTypeException(error_msg)
+		elif( not aStr):
+			raise InvalidTypeException("Parameter was an empty string.")
+	
+	@staticmethod
+	def _dict_of_character_count(self,dict, str, i):
+		value = dict.get(str[i],0)
+		dict[str[i]] = value + 1
+
+	@classmethod
+	def get_duplicate_chars(self,aStr):
+		# Make sure type is 'str'
+		self._checkInput(aStr)
+
+		duplicate_chars = ""
+
+		for i in xrange(0,len(aStr)):
+			for j in xrange(i, len(aStr)):
+				if(aStr[i] == aStr[j] and i != j):
+					duplicate_chars += aStr[i]
+
+		return duplicate_chars
+		# Make sure it returns a str
+		# 
+	
+	@classmethod	
+	def are_anagrams(self,str1,str2):
+		import re 
+
+		map(self._checkInput, [str1,str2])
+
+		# Strips anythings thats not a number, a word or underscore from the parameters
+		str1 = re.sub("\W+", '', str1)
+		str2 = re.sub("\W+", '', str2)
+
+		# Make sure its lower case and strip it of spaces
+		str1 = str1.lower().strip()
+		str2 = str2.lower().strip()
+
+		if (len(str1) != len(str2)):
+			return False
+
+		dict1_CharacterCount = {}
+		dict2_CharacterCount = {}
+
+		#this method is created for the sake of modularity.
+		def dict_of_character_count(dict, str, i):
+				value = dict.get(str[i],0)
+				dict[str[i]] = value + 1
+
+		for x in xrange(len(str1)):
+			dict_of_character_count(dict1_CharacterCount, str1, x)
+			dict_of_character_count(dict2_CharacterCount, str2, x)
+
+		del dict_of_character_count
+
+		for key1,value1 in dict1_CharacterCount.iteritems():
+			value2 = dict2_CharacterCount.get(key1,0)
+			if(value1 != value2):
+				return False
+
+		return True
+		# Now 
+	
+
+	@classmethod
+	def get_first_unique_character(self, aStr):
+		self._checkInput(aStr)
+
+		aStr = aStr.lower() 
+
+		# Omg i'm making these these list comprehensions 
+		# look ugly :O
+		aStr_count_dict = {}
+		[self._dict_of_character_count(self,aStr_count_dict, aStr, i) for i in range(len(aStr))]
+
+		for char in aStr:
+			if(aStr_count_dict[char] == 1):
+				return char
+
+		return ""
+
+
+
+
+
+if __name__ == '__main__':
+
+	a = StringsQ()
+
+	print StringsQ.get_first_unique_character("Saywhat")
 
